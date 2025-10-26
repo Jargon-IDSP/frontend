d192de3cced9bdb6525d0574ff7cb19e15e832d6
KamilBozz <kbozkurt@my.bcit.ca>
2025-10-25 19:30:33 -0700

    Add stylings to documents page and more

--- src/lib/documentRoutes.tsx
+++ src/lib/documentRoutes.tsx
@@ -4,6 +4,7 @@ import DocumentsPage from '../pages/documents/DocumentsPage';
 import AddDocumentPage from '../pages/documents/AddDocumentPage';
 import FullTranslationView from '../pages/documents/FullTranslationView';
 import DocumentStudy from '../pages/documents/DocumentStudy';
+import FilePreviewPage from '../pages/documents/FilePreviewPage';
 
 const protect = (element: React.ReactElement) => <ProtectedRoute>{element}</ProtectedRoute>;
 
@@ -11,6 +12,10 @@ export const documentRoutes: RouteObject[] = [
   {
     path: '/documents',
     element: protect(<DocumentsPage />),
+  },
+  {
+    path: '/documents/preview',
+    element: protect(<FilePreviewPage />),
   },
   {
     path: '/documents/add',
