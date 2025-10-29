1918f6dff045cabe92ef5b79656da8fb65d5e0fd
Robyn <downie.robyn@hotmail.co.uk>
2025-10-28 19:42:16 -0700

    new redireect page

---
+++ src/pages/learning/DemoTakeQuiz.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import goBackIcon from '../../assets/icons/goBackIcon.svg';
+import StudyTypeCard from '../../components/learning/studyType';
+import readBook from '../../assets/readBook.png';
+import demoTermCard from '../../assets/demoTermCard.png';
+import studyLaptop from '../../assets/studyLaptop.png';
+
+export default function DemoTakeQuiz() {
+  const navigate = useNavigate();
+  const quizId = 'cd81eda1-ea47-4b09-9ab1-d47284aefa0f';
+  const docId = 'cmha47br90001ztmchu5awy17';
+
+  const handleStartQuiz = () => {
+    navigate(`/learning/custom/quiz/take?quizId=${quizId}`);
+  };
+
+  const handleTerminology = () => {
+    navigate(`/learning/documents/${docId}/terms`);
+  };
+
+  return (
+    <div className="demo">
+    <img src={goBackIcon} alt="back button" />
+      <h1>level-2-acronyms.pdf</h1>
+      <h1>...</h1>
+
+      <button>Lesson</button>
+      <button>Document</button>
+
+      <img src={demoTermCard} alt="Demo Term Card" />
+
+      <StudyTypeCard
+        name="Terminology"
+        img={readBook}
+        start_button_text="Start Learning"
+        onClick={handleTerminology}
+      />    
+
+      <StudyTypeCard
+        name="Quiz"
+        img={studyLaptop}
+        start_button_text="Start Quiz"
+        onClick={handleStartQuiz}
+      />
+    </div>
+  );
+}
