7730e3e711441630f3178d7c55d24ff4722b6d2f
Ben Chan <bchan158@my.bcit.ca>
2025-11-03 23:43:19 -0800

    Add container className to the DocumentDrawer.tsx

--- src/pages/drawers/DocumentDrawer.tsx
+++ src/pages/drawers/DocumentDrawer.tsx
@@ -16,33 +16,35 @@ export default function DocumentDrawer({
   onOpenChange,
 }: DocumentDrawerProps) {
   return (
-    <Drawer open={open} onOpenChange={onOpenChange} direction="bottom">
-      <DrawerContent>
-        <DrawerFooter>
-          <Button
-            variant="outline"
-            className="border-orange-500 text-orange-500 hover:bg-orange-50"
-          >
-            Translate Document
-          </Button>
-          <Button
-            variant="outline"
-            className="border-orange-500 text-orange-500 hover:bg-orange-50"
-          >
-            Generate a lesson
-          </Button>
-          <Button className="bg-[#6F2E17] text-white hover:bg-orange-90 my-4">
-            Both
-          </Button>
-          {/* Horizontal divider */}
-          <hr className="my-2 border-gray-200 mb-4" />
-          <DrawerClose asChild>
-            <Button variant="outline" className="text-red-500">
-              Cancel
+    <div className="container">
+      <Drawer open={open} onOpenChange={onOpenChange} direction="bottom">
+        <DrawerContent>
+          <DrawerFooter>
+            <Button
+              variant="outline"
+              className="border-orange-500 text-orange-500 hover:bg-orange-50"
+            >
+              Translate Document
             </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+            <Button
+              variant="outline"
+              className="border-orange-500 text-orange-500 hover:bg-orange-50"
+            >
+              Generate a lesson
+            </Button>
+            <Button className="bg-[#6F2E17] text-white hover:bg-orange-90 my-4">
+              Both
+            </Button>
+            {/* Horizontal divider */}
+            <hr className="my-2 border-gray-200 mb-4" />
+            <DrawerClose asChild>
+              <Button variant="outline" className="text-red-500">
+                Cancel
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
   );
 }
